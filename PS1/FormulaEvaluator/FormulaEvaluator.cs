999998f8e3e21bddd21624930f8122a13031c1ce
Christopher Swindle <cjswindle@gmail.com>
2016-09-04 21:19:56 -0600

    added delegate and proper parameters for Evaluate constructor

--- PS1/FormulaEvaluator/FormulaEvaluator.cs
+++ PS1/FormulaEvaluator/FormulaEvaluator.cs
@@ -8,10 +8,21 @@ namespace FormulaEvaluator
 {
     public static class Evaluator
     {
-        private static Stack<int> valueStack;
-        private static Stack<char> operandStack;
-        private static string expression;
+        public static Stack<int> valueStack;
+        public static Stack<char> operandStack;
+        public static string expression;
+        public delegate int Lookup(String v);
 
+        /// <summary>
+        /// this is the constructor for the static Evaluator class 
+        /// </summary>
+        /// <returns></returns>
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            //TO DO
+
+            return 0;
+        }
 
 
     }
