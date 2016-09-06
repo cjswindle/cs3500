c90f05973618114727e2e622f5e5b8e0252daa54
Christopher Swindle <cjswindle@gmail.com>
2016-09-05 21:49:59 -0600

    Began algorithim and added stack extension class as well as added notes to finish algorithm.

--- PS1/FormulaEvaluator/FormulaEvaluator.cs
+++ PS1/FormulaEvaluator/FormulaEvaluator.cs
@@ -19,16 +19,47 @@ namespace FormulaEvaluator
         /// <returns></returns>
         public static int Evaluate(String exp, Lookup variableEvaluator)
         {
+            int value;
+
             //TO DO
             //Creates an array of substings
             string[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
-            
+            Stack<int> valueStack = new Stack<int>();
+            Stack<char> operandStack = new Stack<char>();
+
             foreach (string t in substrings)
             {
+                //If t is Int
+                if (int.TryParse(t, out value))
+                {
+
+                    if (StackExtensions.isOnTop<char>(operandStack, '*' ) )
+                    {
+                        value = value * valueStack.Pop();
+                    }
+                    if (StackExtensions.isOnTop<char>(operandStack, '/'))
+                    {
+                        value = value * valueStack.Pop();
+                    }
+
+                    valueStack.Push(value);
+                }
+                
+                //if t is a variable
+
+                //if t + or -
+
+                //if t is * or /
+
+                //if t is (
+
+                //if t is )
+
                 //matches for variable
                 Regex.IsMatch(t, @"^\s*[a-zA-Z]+\d+\s*$");
                    
             }
+
             //use regex to check for patterns to check variables
             //check if substrings are valid, if valid place in proper stack.  If unvalid, throw exception
             //if substring is a variable, check to make sure it is proper type...if proper pass to delegate
@@ -38,9 +69,35 @@ namespace FormulaEvaluator
             //once operandStack is empty return value of valueStack
             //if operand stack is not empty and valueStack does have value throw exception
 
-            return 0;
+            return valueStack.Pop();
         }
 
 
+
+
+    }
+
+    /// <summary>
+    /// A class for holding extensions to Stack
+    /// </summary>
+    static class StackExtensions
+    {
+        /// <summary>
+        /// An extention for a Stack that determines if the given value is at the top of the stack.
+        /// Peek is insufficient because it will throw if the stack is empty.
+        /// </summary>
+        /// <typeparam name="T">The generic type placeholder</typeparam>
+        /// <param name="s">The stack (this)</param>
+        /// <param name="val">The value to look for on the top of the stack</param>
+        /// <returns>Returns whether or not val is on the top of the stack</returns>
+        public static bool isOnTop<T>(this Stack<T> s, T val)
+        {
+            //check is empty, then peek and return bool
+            if (s.Count >= 1)
+            {
+                return val.Equals(s.Peek());
+            }
+            return false;
+        }
     }
 }
